57230560375703ca6b1b28b32fb68c7dbb46637f
Владимир Силин <sva@itx24.ru>
2025-01-30 13:04:58 +0700

    fix CI errors with slash in urls

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -5,6 +5,6 @@ from .views import SignUpView, UserDeleteView, UsersListView, UserUpdateView
 urlpatterns = [
     path("", UsersListView.as_view(), name="users"),
     path("create/", SignUpView.as_view(), name="sign_up"),
-    path("<int:pk>/update", UserUpdateView.as_view(), name="user_update"),
-    path("<int:pk>/delete", UserDeleteView.as_view(), name="user_delete"),
+    path("<int:pk>/update/", UserUpdateView.as_view(), name="user_update"),
+    path("<int:pk>/delete/", UserDeleteView.as_view(), name="user_delete"),
 ]
